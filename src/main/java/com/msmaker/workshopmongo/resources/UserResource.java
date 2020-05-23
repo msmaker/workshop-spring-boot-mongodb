724118add59252290ce3a8e63043d9ee1257c566
msMaker <marcosilva.tec@gmail.com>
2020-05-23 11:51:53 -0300

    Inserção de usuário com POST

--- src/main/java/com/msmaker/workshopmongo/resources/UserResource.java
+++ src/main/java/com/msmaker/workshopmongo/resources/UserResource.java
@@ -1,18 +1,22 @@
 package com.msmaker.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.msmaker.workshopmongo.domain.User;
 import com.msmaker.workshopmongo.dto.UserDTO;
 import com.msmaker.workshopmongo.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -33,6 +37,14 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findbyId(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
